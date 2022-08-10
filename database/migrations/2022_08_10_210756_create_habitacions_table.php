7adf8f750e9043bdf1f33380fad2cad887860cea
camrweb <camr123yt.yt@gmail.com>
2022-08-10 17:01:51 -0500

    10/08/2022

--- database/migrations/2022_08_10_210756_create_habitacions_table.php
+++ database/migrations/2022_08_10_210756_create_habitacions_table.php
@@ -17,6 +17,7 @@ class CreateHabitacionsTable extends Migration
             $table->id();
             $table->string('price');
             $table->string('description');
+            $table->string('person-max');
             $table->unsignedBigInteger('categoria_id');
             $table->foreign('categoria_id')
                 ->references('id')->on('categorias');
