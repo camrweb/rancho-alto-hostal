43828ac9959afa8bfe43d9d76777f13365cff3d9
camrweb <camr123yt.yt@gmail.com>
2022-08-10 23:13:00 -0500

    10/08/2022

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Iniciar sesión') }}</div>
+                <div class="card-header">{{ __('Inican') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
